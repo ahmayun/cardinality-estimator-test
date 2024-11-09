302570e9b7f6f2fbb08c6415461165e2a875692a
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2024-11-09 18:05:25 -0500

    Add JNI load test

--- src/main/scala/misc/Test.scala
+++ src/main/scala/misc/Test.scala
@@ -1,8 +1,14 @@
 package misc
 
+import sqlsmith.loader.SQLSmithLoader
+
 object Test {
 
   def main(args: Array[String]): Unit = {
+    lazy val sqlSmithApi = SQLSmithLoader.loadApi()
+    val sqlSmithSchema = sqlSmithApi.schemaInit("", 0)
+
     println(s"Test ran with ${args.mkString("Array(", ", ", ")")}")
+    println(s"Successfully loaded sqlsmith JNI ${sqlSmithSchema}")
   }
 }
