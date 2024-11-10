6d4aa9b8a823ac21fea0ca92ab65852d6915558c
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2024-11-10 14:24:30 -0500

    sync

--- src/main/scala/gendata/RunTPCDSGenCluster.scala
+++ src/main/scala/gendata/RunTPCDSGenCluster.scala
@@ -4,7 +4,9 @@ object RunTPCDSGenCluster {
 
   def main(args: Array[String]): Unit = {
     TPCDSGenData.main(Array(
-      ""
+      "spark://zion-headnode:7077",
+      "hdfs://tpcds/",
+      "/home/student/spark-opt/tpcds-kit/tools"
     ))
   }
 }
