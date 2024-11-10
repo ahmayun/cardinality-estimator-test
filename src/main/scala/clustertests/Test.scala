64a8fd229f008245cbb43688be1911355dc15ec6
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2024-11-10 13:41:29 -0500

    sync

--- src/main/scala/clustertests/Test.scala
+++ src/main/scala/clustertests/Test.scala
@@ -14,18 +14,18 @@ object Test {
 
     println("Attempting to create external table to check hive setup...")
 
-//    val spark = SparkSession.builder()
-//      .appName("HiveCheck")
-//      .config("spark.sql.cbo.enabled", "true")
-//      .config("spark.sql.cbo.joinReorder.enabled", "true")
-//      .config("spark.sql.statistics.size.autoUpdate.enabled", "true")
-//      .config("spark.sql.statistics.histogram.enabled", "true")
-//      .master("local[*]")
-//      .enableHiveSupport()
-//      .getOrCreate()
-//    spark.sparkContext.setLogLevel("ERROR")
+    val spark = SparkSession.builder()
+      .appName("HiveCheck")
+      .config("spark.sql.cbo.enabled", "true")
+      .config("spark.sql.cbo.joinReorder.enabled", "true")
+      .config("spark.sql.statistics.size.autoUpdate.enabled", "true")
+      .config("spark.sql.statistics.histogram.enabled", "true")
+      .master("local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
 
-//    spark.sql("select * from call_center").show(5)
+    spark.sql("select * from call_center").show(5)
 
   }
 }
