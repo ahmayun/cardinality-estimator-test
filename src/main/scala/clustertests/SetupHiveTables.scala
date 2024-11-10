65fb4a88e532f7cbd4efc21e8fbb32bac1af58d8
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2024-11-10 13:23:27 -0500

    Refactor and add lib/ dir

---
+++ src/main/scala/clustertests/SetupHiveTables.scala
@@ -0,0 +1,24 @@
+package clustertests
+
+import org.apache.spark.sql.SparkSession
+
+object SetupHiveTables {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("HiveCheck")
+      .config("spark.sql.cbo.enabled", "true")
+      .config("spark.sql.cbo.joinReorder.enabled", "true")
+      .config("spark.sql.statistics.size.autoUpdate.enabled", "true")
+      .config("spark.sql.statistics.histogram.enabled", "true")
+      .master("local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    spark.sql("CREATE DATABASE IF NOT EXISTS main")
+    spark.sql("CREATE EXTERNAL TABLE IF NOT EXISTS customer (id INT, name STRING, ...)\nSTORED AS PARQUET\nLOCATION 'hdfs://path/to/customer';")
+
+  }
+
+}
---
+++ src/main/scala/clustertests/Test.scala
@@ -0,0 +1,31 @@
+package clustertests
+
+import org.apache.spark.sql.SparkSession
+import sqlsmith.loader.SQLSmithLoader
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    lazy val sqlSmithApi = SQLSmithLoader.loadApi()
+    val sqlSmithSchema = sqlSmithApi.schemaInit("", 0)
+
+    println(s"Test ran with ${args.mkString("Array(", ", ", ")")}")
+    println(s"Successfully loaded sqlsmith JNI ${sqlSmithSchema}")
+
+    println("Attempting to create external table to check hive setup...")
+
+//    val spark = SparkSession.builder()
+//      .appName("HiveCheck")
+//      .config("spark.sql.cbo.enabled", "true")
+//      .config("spark.sql.cbo.joinReorder.enabled", "true")
+//      .config("spark.sql.statistics.size.autoUpdate.enabled", "true")
+//      .config("spark.sql.statistics.histogram.enabled", "true")
+//      .master("local[*]")
+//      .enableHiveSupport()
+//      .getOrCreate()
+//    spark.sparkContext.setLogLevel("ERROR")
+
+//    spark.sql("select * from call_center").show(5)
+
+  }
+}
